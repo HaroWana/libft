3f823c657aea4ffd4a80f4c0627da569af22baa1
HaroWana <johann.lorber@gmail.com>
2024-04-14 14:57:11 +0200

    -additional formatting in libft.h

--- libft.h
+++ libft.h
@@ -11,19 +11,19 @@ typedef struct	s_list
 	struct s_list	*next;
 }				t_list;
 
-int		ft_isalpha(int c);
+int	ft_isalpha(int c);
 
-int		ft_isdigit(int c);
+int	ft_isdigit(int c);
 
-int		ft_isalnum(int c);
+int	ft_isalnum(int c);
 
-int		ft_isascii(int c);
+int	ft_isascii(int c);
 
-int		ft_isprint(int c);
+int	ft_isprint(int c);
 
 void	*ft_calloc(size_t nmemb, size_t size);
 
-int		ft_atoi(const char *nptr);
+int	ft_atoi(const char *nptr);
 
 char	*ft_itoa(int n);
 
@@ -37,7 +37,7 @@ void	*ft_memmove(void *dst, const void *src, size_t n);
 
 void	*ft_memchr(const void *s, int c, size_t n);
 
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int	ft_memcmp(const void *s1, const void *s2, size_t n);
 
 char	**ft_split(char const *s, char c);
 
@@ -47,7 +47,7 @@ char	*ft_strrchr(const char *s, int c);
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t n);
 
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
 
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 
@@ -67,9 +67,9 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 
-int		ft_toupper(int c);
+int	ft_toupper(int c);
 
-int		ft_tolower(int c);
+int	ft_tolower(int c);
 
 void	ft_putchar_fd(char c, int fd);
 
@@ -81,7 +81,7 @@ void	ft_putnbr_fd(int n, int fd);
 
 t_list	*ft_lstnew(void *content);
 
-t_list	*ft_lstlast(t_list *lst);
+t_list		*ft_lstlast(t_list *lst);
 
 void	ft_lstadd_back(t_list **list, t_list *new);
 
@@ -95,6 +95,6 @@ void	ft_lstadd_front(t_list **lst, t_list *new);
 
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 
-int		ft_lstsize(t_list *lst);
+int	ft_lstsize(t_list *lst);
 
 #endif
